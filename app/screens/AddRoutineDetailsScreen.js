f3c5d029c408cf354f5d0a6a8e225c1e924eb9d1
rahul-tuladhar <rt4hc@virginia.edu>
2019-07-20 17:28:18 -0400

    Added text input to add routine

--- app/screens/AddRoutineDetailsScreen.js
+++ app/screens/AddRoutineDetailsScreen.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
-import { View, ScrollView, Text } from 'react-native';
+import { View, ScrollView, Text, TextInput } from 'react-native';
 import { Card } from 'react-native-elements';
 
 class AddRoutineDetailsScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: 'Some Placeholder text'
+        }
+    }
+    
     render() {
         return (
             <View>
                 <Card
                     featuredTitle={"Create a New Routine"}
                     >
-                <Text style={{margin:10}}>
-                    Click the button below to create a new routine
+                <Text style={{margin:10, justifyContent: 'center'}}>
+                    Create a New Routine:
                 </Text>
+                <TextInput
+                    style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+                    onChangeText={(text) => this.setState({text})}
+                    value={this.state.text}
+                />
                 </Card>
             </View>
         )
